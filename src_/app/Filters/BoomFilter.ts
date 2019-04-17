eb78d2d832d7b236c70b4609ce1f27c590f2af39
s.sugumaran@asos.com <yesoreyeram@gmail.com>
2019-04-17 23:11:47 +0100

    cleanup

--- src_/app/Filters/BoomFilter.ts
+++
@@ -1,8 +0,0 @@
-import { IBoomFilter } from "../../definitions/types";
-import { BoomFilter } from "./Filter";
-
-export class BoomSummaryFilter extends BoomFilter implements IBoomFilter {
-  constructor(options) {
-    super(options);
-  }
-}
